b8250009f495d67b035234d4c716e07dad911bc0
Yddiezon Francisco <3zyfrancisco@gmail.com>
2023-05-13 13:46:02 -0400

    color flipper done

--- projects/color_flipper/app.js
+++ projects/color_flipper/app.js
@@ -1,13 +1,16 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const hex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener('click', function(){
-    const randomNumber = 0
-    document.body.style.backgroundColor = colors[randomNumber]
-    color.textContent = colors[randomNumber]
+    let hexColor="#";
+    for(let i=0; i<6; i++){
+        hexColor += hex[hexRand]
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
 });
 
-function getRandNum(){
-    return Math.random()
+function hexRand(){
+    return Math.floor(Math.random()*hex.length)
 }
